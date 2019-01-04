e59f8d181998abba92f82c3a6bf946d429138a0e
1437763632 <45552902+1437763632@users.noreply.github.com>
2019-01-04 20:37:07 +0800

    wy

---
+++ ChinaBank/ChinaBank.IService/IWeeklyServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinaBank.IService
+{
+    /// <summary>
+    /// 项目周报表
+    /// </summary>
+    public interface IWeeklyServices
+    {
+    }
+}
